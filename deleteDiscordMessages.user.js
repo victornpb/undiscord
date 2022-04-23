e2eaef1db7303a34adc95629efe34cec8e2ee569
Seth Falco <seth@falco.fun>
2022-04-23 18:58:07 +0100

    fix: stricter url matching

--- deleteDiscordMessages.user.js
+++ deleteDiscordMessages.user.js
@@ -3,7 +3,9 @@
 // @description   Extends the discord interface so you can mass delete messages from discord
 // @namespace     https://github.com/victornpb/deleteDiscordMessages
 // @version       4.2
-// @match         https://discord.com/*
+// @match         https://discord.com/app
+// @match         https://discord.com/channels/*
+// @match         https://discord.com/login
 // @downloadURL   https://raw.githubusercontent.com/victornpb/deleteDiscordMessages/master/deleteDiscordMessages.user.js
 // @homepageURL   https://github.com/victornpb/deleteDiscordMessages
 // @supportURL    https://github.com/victornpb/deleteDiscordMessages/issues
