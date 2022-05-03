6ec0c7e140cc3eaf3d12f43e6efbf7a622de9fc2
victornpb <3372598+victornpb@users.noreply.github.com>
2022-05-03 13:33:51 -0300

    created betterdiscord entry point

---
+++ src/index-bd.js
@@ -0,0 +1,12 @@
+// const { initUI, destroy } = require('./undiscord');
+
+export default class Undiscord {
+  // Required function. Called when the plugin is activated (including after reloads)
+  start() {
+    // initUI();
+  }
+  // Required function. Called when the plugin is deactivated
+  stop() {
+    // destroy();
+  }
+}
