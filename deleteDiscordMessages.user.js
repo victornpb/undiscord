d2aa80e0baa708c7cc5733ef8da4ecd41ea410d3
Titus M <86983072+titushm@users.noreply.github.com>
2023-01-20 12:48:20 +0000

    Update deleteDiscordMessages.user.js

--- deleteDiscordMessages.user.js
+++ deleteDiscordMessages.user.js
@@ -1200,28 +1200,38 @@ body.undiscord-pick-message.after [id^="message-content-"]:hover::after {
   var messagePicker$1 = messagePicker;
   window.messagePicker = messagePicker;
 
-  function getToken() {
-    window.dispatchEvent(new Event('beforeunload'));
-    const LS = document.body.appendChild(document.createElement('iframe')).contentWindow.localStorage;
-    return JSON.parse(LS.token);
-  }
+function getLocalStorage() {
+  var iframe = document.createElement('iframe');
+  document.head.append(iframe);
+  var ls = Object.getOwnPropertyDescriptor(iframe.contentWindow, 'localStorage');
+  iframe.remove();
+  Object.defineProperty(window, 'localStorage', ls);
+  return ls.get.call(window);
+}
 
-  function getAuthorId() {
-    const LS = document.body.appendChild(document.createElement('iframe')).contentWindow.localStorage;
-    return JSON.parse(LS.user_id_cache);
-  }
+export function getToken() {
+  window.dispatchEvent(new Event('beforeunload'));
+  var LS = getLocalStorage();
+  return JSON.parse(LS.token);
+}
 
-  function getGuildId() {
-    const m = location.href.match(/channels\/([\w@]+)\/(\d+)/);
-    if (m) return m[1];
-    else alert('Could not the Guild ID!\nPlease make sure you are on a Server or DM.');
-  }
+export function getAuthorId() {
+  var LS = getLocalStorage();
+  return JSON.parse(LS.user_id_cache);
+}
 
-  function getChannelId() {
-    const m = location.href.match(/channels\/([\w@]+)\/(\d+)/);
-    if (m) return m[2];
-    else alert('Could not the Channel ID!\nPlease make sure you are on a Channel or DM.');
-  }
+export function getGuildId() {
+  const m = location.href.match(/channels\/([\w@]+)\/(\d+)/);
+  if (m) return m[1];
+  else alert('Could not the Guild ID!\nPlease make sure you are on a Server or DM.');
+}
+
+export function getChannelId() {
+  const m = location.href.match(/channels\/([\w@]+)\/(\d+)/);
+  if (m) return m[2];
+  else alert('Could not the Channel ID!\nPlease make sure you are on a Channel or DM.');
+}
+
 
   // ------------------------- User interface ------------------------------ //
 
