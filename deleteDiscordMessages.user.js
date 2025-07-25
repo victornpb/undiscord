da6c01e49f702956be64e9d0e496fa0454c774b2
gaveitatry2 <75463665+gaveitatry2@users.noreply.github.com>
2025-07-25 05:38:41 -0500

    Update deleteDiscordMessages.user.js
    For over a year, people have been experiencing 401 errors because Undiscord is auto filling the authentication token text box with the user's Discord password. This is very alarming because that is a security flaw. The fix is very simple. Replace the word "password" with the word "text". Now, Undiscord will work without any 401 errors and the user can feel safer knowing that their Discord password isn't being randomly inserted somewhere.
    
    In addition to that, people supposedly had an issue where Undiscord would stop even though there were messages remaining that needed to be deleted.
    
    Someone's proposed fix was to add:
    const messagesRemaining = this.state.grandTotal - this.state.offset;
    
    and then add a log.verb message to go along with it. Seems like a reasonable small, simple, and useful fix.

--- deleteDiscordMessages.user.js
+++ deleteDiscordMessages.user.js
@@ -570,6 +570,8 @@
 	      this.calcEtr();
 	      log.verb(`Estimated time remaining: ${msToHMS(this.stats.etr)}`);
 
+	      const messagesRemaining = this.state.grandTotal - this.state.offset;
+
 	      // if there are messages to delete, delete them
 	      if (this.state._messagesToDelete.length > 0) {
 
@@ -588,6 +590,9 @@
 	        log.verb('There\'s nothing we can delete on this page, checking next page...');
 	        log.verb(`Skipped ${this.state._skippedMessages.length} out of ${this.state._seachResponse.messages.length} in this page.`, `(Offset was ${oldOffset}, ajusted to ${this.state.offset})`);
 	      }
+	      else if (this.state.delCount < messagesRemaining) {
+	        log.verb('There\'s messages remaining, checking next page...');
+	      }		      
 	      else {
 	        log.verb('Ended because API returned an empty page.');
 	        log.verb('[End state]', this.state);
